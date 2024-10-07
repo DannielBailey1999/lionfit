ec23a72fd797b5e0802d8d0ee5be3395ab3c361f
unknown <dannielbailey1999@gmail.com>
2024-10-07 15:50:31 -0500

    Search Bar

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,7 @@
 
-import { Text, View, StyleSheet, FlatList} from "react-native";
+import { Text, View, StyleSheet, FlatList, TextInput, Button} from "react-native";
 import FoodListItem from "../src/components/foodListItem";
-
-
+import { useState } from "react";
 
 const foodItems = [
   {label: "Pizza", cal: 75, brand: 'Dominoes'}, 
@@ -13,15 +12,28 @@ const foodItems = [
 
 ];
 export default function Index() {
+  const [search, setSearch] = useState('');
+  const performSearch = () => {
+    console.warn('Searching for: ', search)
+    setSearch('');
+  }
   return (
     <View
       style={styles.container}>
         {/* Food View Item  */}
+        <TextInput 
+        value = {search}
+        onChangeText= {setSearch}
+        placeholder="Search..." 
+        style={styles.input}/>
+        {search && <Button title="Search" onPress={performSearch}/>}
+
       <FlatList 
       data = {foodItems}
       renderItem={({item}) => <FoodListItem item = {item} />}
       contentContainerStyle={{gap: 5}}
       />
+      
     </View>
   );
 }
@@ -29,9 +41,13 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     padding: 10,
     gap: 5,
+  },
+  input: {
+    backgroundColor: '#F2F2F2',
+    padding: 10, 
+    borderRadius: 10, 
   },
 
 })
